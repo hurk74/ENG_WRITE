997b0ed0546be53187bd482a77fe6f93f705ca8e
hurk74 <143951301+hurk74@users.noreply.github.com>
2023-09-04 14:28:49 +0900

    Add files via upload

---
+++ blogInfo.py
@@ -0,0 +1,4 @@
+# tstory API 정보
+access_token = "6b5413ecb66d8fa91bcc6b922093e5f5_29d668931243a126cb78df72d78ed478"
+blogName = "dongjunhanip"
+categoryId = "1139134"
---
+++ papagoID.py
@@ -0,0 +1,2 @@
+client_id = "snV5_QFHYyvqUWVmp3Cp"
+client_secret = "cd2FWeKEmQ"
